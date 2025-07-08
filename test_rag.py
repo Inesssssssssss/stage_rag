3a731658c730f91d30635ec984d4baf25b4ab4c9
rahaoui <rahaoui@isir.upmc.fr>
2025-07-08 15:04:01 +0000

    premier test rag

---
+++ test_rag.py
@@ -0,0 +1,55 @@
+import ollama
+import chromadb
+
+documents = [
+  "Llamas are members of the camelid family meaning they're pretty closely related to vicuñas and camels",
+  "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands",
+  "Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall",
+  "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight",
+  "Llamas are vegetarians and have very efficient digestive systems",
+  "Llamas live to be about 20 years old, though some only live for 15 years and others live to be 30 years old",
+]
+
+client = chromadb.Client()
+collection = client.create_collection(name="docs")
+
+# store each document in a vector embedding database
+for i, d in enumerate(documents):
+  response = ollama.embed(model="mxbai-embed-large", input=d)
+  embeddings = response["embeddings"]
+  collection.add(
+    ids=[str(i)],
+    embeddings=embeddings,
+    documents=[d]
+  )
+
+# an example input
+#input = "What animals are llamas related to?"
+input = "Where is Paris?"
+# generate an embedding for the input and retrieve the most relevant doc
+response = ollama.embeddings(
+      prompt=input,
+      model="mxbai-embed-large"
+    )
+results = collection.query(
+  query_embeddings=[response["embedding"]],
+  n_results=2
+)
+data = results['documents'][0]
+print(f" data : {results['documents'][0]}")
+
+output_no_rag = ollama.generate(
+  model="llama3.1:8B",
+  prompt=f"Respond to this prompt: {input}"
+)
+
+# generate a response combining the prompt and data we retrieved in step 2
+output = ollama.generate(
+  model="llama3.1:8B",
+  prompt=f"Using this data : {data}. Respond to this prompt: {input}"
+)
+
+print(f"RAG answer : {output['response']}\n")
+
+print(f"without RAG :{output_no_rag['response']}")
+

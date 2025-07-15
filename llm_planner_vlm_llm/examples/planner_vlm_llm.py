14b31aa97be8d4f2a5bbdb4d29aa6378efa7dc14
rahaoui <rahaoui@isir.upmc.fr>
2025-07-15 18:21:49 +0200

    input rag

--- llm_planner_vlm_llm/examples/planner_vlm_llm.py
+++ llm_planner_vlm_llm/examples/planner_vlm_llm.py
@@ -1,79 +1,35 @@
 from llm_planner_vlm_llm.task_planner import TaskPlanner
+from llm_planner_vlm_llm.utils.other import get_useful_doc, get_image, get_draft
 import time
 import chromadb
 import ollama
 import re
 
-def get_useful_doc(scollection,task):
-    """
-    Find the most useful information in the documents
-    """
-    response = ollama.embeddings(
-        prompt=task,
-        model="mxbai-embed-large"
-        )
-    results = collection.query(
-        query_embeddings=[response["embedding"]],
-        n_results=10
-    )
-    # Generate a threshold to filter relevant documents ( thresold can be adjusted)
-    threshold = 0.9
-    relevant_docs = []
-    for doc, dist in zip(results["documents"][0], results["distances"][0]):
-        if dist <= threshold:
-            relevant_docs.append(doc)
-    return relevant_docs
-
-
-# Set of example documents to be used for rag
-documents=['The black gloves belong to bob',
-           'The fan is broken',
-           'We can still use the white cloth']
-
+# Initialize the ChromaDB client
 client = chromadb.Client()
 collection = client.create_collection(name="docs", metadata={"hnsw:space": "cosine"})
 
-# store each document in a vector embedding database
-for i, d in enumerate(documents):
-  response = ollama.embed(model="mxbai-embed-large", input=d)
-  embeddings = response["embeddings"]
-  collection.add(
-    ids=[str(i)],
-    embeddings=embeddings,
-    documents=[d]
-  )
+# Set of example documents to be used for rag
+documents=["The blue block is owned by Bob"]
 
 # Generate a vision response for the image
+get_image()
 response = ollama.generate(
     model='llama3.2-vision',
-    prompt= 'You are a robot assistant. Please analyze the object in the image. If you see a robot arm in the picture, ignore it and focus on the object. Be concise.',
-    images= ['../Images/55.jpg']
+    prompt= 'You are a robot assistant. Please analyze the object on the table in the image. If you see a robot arm or a table in the picture , ignore it and focus on the object. Be concise.',
+    images= ['Images/live.png']
 )
 im_desc = response.get("response", "")
+print(f"Image description: {im_desc}")
 
 # Initialize the task planner
 planner = TaskPlanner(
     model_name="qwen3:4b",  # Specify the model you have in Ollama
 )
 task = f"Put this object ({im_desc}) somewhere"
+docs = []
 
-docs = get_useful_doc(collection, task)
-print(f"Useful documents for the task : {docs}")
-
-prompt = f"""You are a fixed robotic arm equipped with a gripper. You can place objects into three distinct boxes:
-
-- Box 1: for personal objects  
-- Box 2: for tools  
-- Box 3: for trash
-
-You can use the information provided in a Python-style list: {docs}  
-Try to link related pieces of information together when they refer to similar or identical objects or concepts.
-
-Your task is the following: {task}  
-Explain **simply and concisely** what you would do to complete this task, step by step, at a high level.  
-The goal is to produce a draft plan of meaningful, human-level actions (e.g., "Grasp screwdriver", "Put pen in Box 1", etc.).
-
-Keep your explanation brief and focused. Do not include implementation details or low-level actions."""
+prompt = get_draft(docs, task, im_desc)
 start_time = time.time()
 response = ollama.generate(
     model='qwen3:4b',
@@ -81,7 +37,35 @@ response = ollama.generate(
 )
 response = re.sub(r'<think>.*?</think>\s*', '', response.get('response', ''), flags=re.DOTALL)
 print(f"Response from the model: {response}")
+print("Do you wanna add something ?")
+rep = input("Type no if the plan is correct")
+if rep.lower() != 'no':
+    documents.append(rep)
 
+    # store each document in a vector embedding database
+    for i, d in enumerate(documents):
+        emb = ollama.embed(model="mxbai-embed-large", input=d)
+        embeddings = emb["embeddings"]
+        collection.add(
+            ids=[str(i)],
+            embeddings=embeddings,
+            documents=[d]
+        )
+
+    docs = get_useful_doc(collection, task)
+    print(f"Useful documents for the task: {docs}")
+    prompt = get_draft(docs, task, im_desc)
+
+    response = ollama.generate(
+        model='qwen3:4b',
+        prompt=prompt,
+        options={
+                    "temperature": 0.0,
+                    "num_predict": 1024
+                }
+    )
+    response = re.sub(r'<think>.*?</think>\s*', '', response.get('response', ''), flags=re.DOTALL)
+    print(f"Response from the model after adding: {response}")
 # Generate a plan for a task
 start_time = time.time()
 # We add collection to choose the best information in high_level_prompt
